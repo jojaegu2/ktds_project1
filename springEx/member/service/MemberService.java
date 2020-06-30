5fa7fdf3dcdfd257905bb71439e8f7cb5b6987e7
jojaegu2 <65579171+jojaegu2@users.noreply.github.com>
2020-06-30 16:16:09 +0900

    Add files via upload

---
+++ springEx/member/service/MemberService.java
@@ -0,0 +1,13 @@
+package com.myspring.springEx.member.service;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+import com.myspring.springEx.member.vo.MemberVO;
+
+public interface MemberService {
+	 public List listMembers() throws DataAccessException;
+	 public int addMember(MemberVO memberVO) throws DataAccessException;
+	 public int removeMember(String id) throws DataAccessException;
+	 public MemberVO login(MemberVO memberVO) throws Exception;
+}
